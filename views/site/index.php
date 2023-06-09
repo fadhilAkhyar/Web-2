f368109aa04273b8d9a24c988a51a617cb321250
fadhilAkhyar <fdhlakhyr@gmail.com>
2023-06-09 15:10:48 +0800

    first commit

--- views/site/index.php
+++ views/site/index.php
@@ -8,7 +8,7 @@ $this->title = 'My Yii Application';
   <section id="hero">
     <div class="hero-container">
       <h1>Welcome to Amoeba</h1>
-      <h2>We are team of talented designers making websites with Bootstrap</h2>
+      <h2>We are team of talented designers making websites</h2>
       <a href="#about" class="btn-get-started scrollto">Get Started</a>
     </div>
   </section><!-- #hero -->
